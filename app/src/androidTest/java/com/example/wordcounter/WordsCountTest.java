4d85ce07704f6abd7d578413c0fa1d0bb79f577a
LargeOof <pashasanius@gmail.com>
2021-12-23 14:39:04 +0200

    Unit and UI tests added

---
+++ app/src/androidTest/java/com/example/wordcounter/WordsCountTest.java
@@ -0,0 +1,108 @@
+package com.example.wordcounter;
+
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import static androidx.test.InstrumentationRegistry.getInstrumentation;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+
+import com.example.wordcounter.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class WordsCountTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void wordsCountTest() {
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.edPhraseInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("hey there"), closeSoftKeyboard());
+
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.spCountingOption),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatSpinner.perform(click());
+
+        DataInteraction appCompatCheckedTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        appCompatCheckedTextView.perform(click());
+
+        ViewInteraction materialButton = onView(
+                allOf(withId(R.id.btnCount), withText("Calculate"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        materialButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.tvResult), withText("2"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("2")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
--- app/src/test/java/com/example/wordcounter/ExampleUnitTest.java
+++ app/src/test/java/com/example/wordcounter/ExampleUnitTest.java
@@ -4,14 +4,109 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+import com.example.wordcounter.utils.TextCounter;
+
 public class ExampleUnitTest {
+    //Testing of words counter
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void wordsCountIsCorrect1() {
+        String inputString = "";
+
+        int expectedValue = 0;
+        int actualValue = TextCounter.countWords(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void wordsCountIsCorrect2() {
+        String inputString = "hello, can you count the words correctly?";
+
+        int expectedValue = 7;
+        int actualValue = TextCounter.countWords(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void wordsCountIsCorrect3() {
+        String inputString = "     ";
+
+        int expectedValue = 0;
+        int actualValue = TextCounter.countWords(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void wordsCountIsCorrect4() {
+        String inputString = "hello";
+
+        int expectedValue = 1;
+        int actualValue = TextCounter.countWords(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void wordsCountIsCorrect5() {
+        String inputString = ", . ? !";
+
+        int expectedValue = 0;
+        int actualValue = TextCounter.countWords(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    //Testing of chars counter
+
+    @Test
+    public void charsCountIsCorrect1() {
+        String inputString = "";
+
+        int expectedValue = 0;
+        int actualValue = TextCounter.getCharsCount(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void charsCountIsCorrect2() {
+        String inputString = "  "; //two spaces
+
+        int expectedValue = 2;
+        int actualValue = TextCounter.getCharsCount(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void charsCountIsCorrect3() {
+        String inputString = "      "; //two tabs
+
+        int expectedValue = 6;
+        int actualValue = TextCounter.getCharsCount(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void charsCountIsCorrect4() {
+        String inputString = "hello,!?";
+
+        int expectedValue = 8;
+        int actualValue = TextCounter.getCharsCount(inputString);
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void charsCountIsCorrect5() {
+        String inputString = ",.!&&64";
+
+        int expectedValue = 7;
+        int actualValue = TextCounter.getCharsCount(inputString);
+
+        assertEquals(expectedValue, actualValue);
     }
 }
